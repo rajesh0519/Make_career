c18a0e1a3ba71280e327ff39707f231cac7fba32
Rajesh Rawat <rawatrajesh0519@gmail.com>
2022-03-22 17:47:41 +0530

    version 0.1

--- pages/templates/template1.js
+++ pages/templates/template1.js
@@ -1,13 +1,6 @@
 // import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
-import React , {useRef} from 'react'
 
 export const Template1 = React.forwardRef(({data}, ref) => {
-
-
-const checkskill = (datas) => {
- 
-    return !datas.includes("Intermediate") & !datas.includes("Beginner") & !datas.includes("Expert") & datas !== ""
-    }
 
     return(
         <>
